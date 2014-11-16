f03b9992bbed03f40d85fb43ee52fc306cda14bd
kyesh <kyesh@users.noreply.github.com>
2014-11-16 02:20:48 +0100

    Basic Scanning should be working. Still need to test

--- gen/se/hj/androidgroupa2/R.java
+++ gen/se/hj/androidgroupa2/R.java
@@ -25,18 +25,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int scan_button=0x7f080000;
+        public static final int scan_content=0x7f080002;
+        public static final int scan_format=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_barcode_scanner=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int barcode_scanner=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int scan=0x7f050004;
+        public static final int title_activity_barcode_scanner=0x7f050003;
     }
     public static final class style {
         /** 
